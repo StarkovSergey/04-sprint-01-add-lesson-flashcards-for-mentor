4da8ac6c2aedf8e21188bc3f61ebdf8252a81a55
Starkov Sergey <starkovsr@gmail.com>
2023-08-27 15:13:08 +0300

    task 03*. add custom hook

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,17 +1,9 @@
 import s from './DecksList.module.css'
-import { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
-import { selectDecks } from '../decks-selectors.ts'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
-import { fetchDecksThunk } from '../decks-thunks.ts'
+import { useFetchDecks } from './useFetchDecks.ts'
 
 export const DecksList = () => {
-  const dispatch = useAppDispatch()
-  const decks = useAppSelector(selectDecks)
-
-  useEffect(() => {
-    dispatch(fetchDecksThunk())
-  }, [])
+  const { decks } = useFetchDecks()
 
   return (
     <ul className={s.list}>
